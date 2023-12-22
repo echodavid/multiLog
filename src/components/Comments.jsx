a22a1146dab7dfd94a4ae2fccf1fd822b1d92bee
Daniela Espinoza <danes@roandai.com>
2023-12-22 13:06:42 -0600

    commit

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
-
+// import './Comments.css'; // Importa el archivo de estilos CSS
 
 const Comments = ({ user }) => {
   return (
     <div className="comments container">
-      <h1>Comentarios de {user.username}</h1>
+      <h1>Crea una publicación {user.username}</h1>
+      <form>
+        <textarea className="input-text" placeholder="Escribe tu publicación"></textarea>
+        <input type="file" className="input-file" accept="image/*" />
+        <button className="submit-button">Publicar</button>
+      </form>
     </div>
   );
 };
